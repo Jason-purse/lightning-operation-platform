0d7fe780689171807d3a67335acb115b2e594872
jasonj <617229004@qq.com>
2023-03-28 20:52:46 +0800

    operation platform

--- operation-platform-front/src/utils/ossClient.js
+++ operation-platform-front/src/utils/ossClient.js
@@ -5,7 +5,7 @@ const client = new OSS({
   // region以杭州为例（oss-cn-hangzhou），其他region按实际情况填写。
   region: 'oss-cn-shanghai',
   // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-  accessKeyId: 'LTAI4GGNxMj1enEqMEyrjyVq',
-  accessKeySecret: 'zf5kU8PWZWuvFRSgEXSPmboAG1jCeu',
+  accessKeyId: 'xxxxx',
+  accessKeySecret: 'xxxxx',
 });
 export default client
